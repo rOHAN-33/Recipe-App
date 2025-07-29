6bda0c25dc40d8b3fe9dd1f1278445f2572dd464
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-07-30 01:02:24 +0530

    completed the backend now moving to frontend

---
+++ backend/readme.md
@@ -0,0 +1,3 @@
+in postgreesql we mainly use drizzle to communicate with the database like CRUD operations
+the service we use is NEON    https://console.neon.tech/app/projects/hidden-sun-56878006?database=neondb
+
--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,8 +1,7 @@
-import "dotenv/config"
-const ENV = {
-    PORT : process.env.PORT,
-    DATABASE_URL : process.env.DATABASE_URL,
-    NODE_ENV:process.env.NODE_ENV
-}
+import "dotenv/config";
 
-export default ENV;
+export const ENV = {
+  PORT: process.env.PORT || 5001,
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+};
